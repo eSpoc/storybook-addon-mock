e84964c870af1358956d8f1807efc73140a6054a
Erez Lirov <elirov@espoc.com>
2025-12-01 14:16:20 -0500

    lint

--- packages/mock-addon/src/manager.ts
+++ packages/mock-addon/src/manager.ts
@@ -3,14 +3,17 @@ import { ADDON_ID, PANEL_ID } from './utils/constants';
 import { Panel } from './components/Panel';
 import { addons, types } from 'storybook/manager-api';
 
-addons.register(ADDON_ID, (api) => {
+addons.register(ADDON_ID, () => {
     // Register the panel
     addons.add(PANEL_ID, {
         type: types.PANEL,
         title: 'Mock Request',
         match: ({ viewMode }) => viewMode === 'story',
         render: (renderProps) => {
-            return React.createElement(Panel, { key: PANEL_ID, ...renderProps });
+            return React.createElement(Panel, {
+                key: PANEL_ID,
+                ...renderProps,
+            });
         },
         paramKey: 'mockAddonConfigs',
     });
